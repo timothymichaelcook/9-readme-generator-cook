b51bc3113a94761516bd9b53af38fd7c53002f69
timothymichaelcook <tmc295@email.vccs.edu>
2023-02-28 19:46:43 -0500

    created function to generate file

--- Develop/index.js
+++ Develop/index.js
@@ -25,7 +25,10 @@ function writeToFile(fileName, data) {
 
 // TODO: Create a function to initialize app
 function init() {
-  
+  inquirer.prompt(questions).then((inquirerResponses) => {
+    console.log('Generating README...');
+    writeToFile('README.md', generateMarkdown({ ...inquirerResponses }));
+  });
 }
 
 // Function call to initialize app
