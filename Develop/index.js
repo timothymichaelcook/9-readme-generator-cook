20cae0e4ba972d2cdf73a7c6ff0ee09c436242a0
timothymichaelcook <tmc295@email.vccs.edu>
2023-02-28 19:43:50 -0500

    added 1st prompt

--- Develop/index.js
+++ Develop/index.js
@@ -5,13 +5,28 @@ const inquirer = require('inquirer');
 const generateMarkdown = require('./utils/generateMarkdown');
 
 // TODO: Create an array of questions for user input
-const questions = [];
+const questions = [
+  {
+  type: 'input',
+  name: 'github',
+  message: 'What is your GitHub username?',
+  },
+  {
+    type: 'input',
+    name: 'github',
+    message: 'What is your GitHub password?',
+    },
+];
 
 // TODO: Create a function to write README file
-function writeToFile(fileName, data) {}
+function writeToFile(fileName, data) {
+  return fs.writeFileSync(path.join(process.cwd(), fileName), data);
+}
 
 // TODO: Create a function to initialize app
-function init() {}
+function init() {
+  
+}
 
 // Function call to initialize app
 init();
